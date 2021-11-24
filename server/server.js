43f149b9004caeb36949b99f2759cb4cd0f16ce9
Anshuman Padiya <53999212+AnshumanPadiya@users.noreply.github.com>
2021-11-25 02:36:09 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Document = require('./models/document');
+
+const defaultValue = "";
+const dburi = process.env.DBURI;
+const port = 3001;
+
+mongoose.connect(dburi, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const io = require("socket.io")(port, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+})
+io.on("connection", socket => {
+
+    socket.on("get-document", async documentId => {
+        const document = await findCreateDocument(documentId);
+        socket.join(documentId);
+        socket.emit("load-document", document.data);
+
+        socket.on("send-changes", delta => {
+            socket.broadcast.to(documentId).emit("receive-changes", delta);
+        })
+
+        socket.on("save-document", async data => {
+            await Document.findByIdAndUpdate(documentId, { data });
+        })
+    })   
+})
+
+async function findCreateDocument(id) {
+    if(id == null) return 
+
+    const document = await Document.findById(id);
+    if(document) return document
+    return await Document.create({ _id: id, data: defaultValue })
+}
